525941841542e23f1e22b8e0f89713ffac022b15
Bob Belderbos <bobbelderbos@gmail.com>
2021-07-29 13:33:55 +0200

    created article model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.url} - {self.user}"
