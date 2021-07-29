cbd3b40d69a5d485ae2d65b72d4a599073a75477
ardarm <arisrooney.09@gmail.com>
2021-07-29 17:10:34 +0700

    add sign-up and login functionality

--- aggregator/settings.py
+++ aggregator/settings.py
@@ -134,3 +134,6 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APIKEY = config('APIKEY', default='')
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from news import views
+from .views import SignUpView
 
 urlpatterns = [
     path('', views.home, name="Home"),
     path("search/", views.search, name="Search"),
     path('next', views.load, name="Next"),
+    path('signup/', SignUpView.as_view(), name='SignUp'),
 ]
